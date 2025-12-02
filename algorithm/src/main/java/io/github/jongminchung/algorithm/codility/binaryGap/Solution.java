389773447e92d62cc3e5bf86b6416ff60769d891
jongminchung <chungjm0711@gmail.com>
2025-12-02 21:04:37 +0900

    feat: binary gap

---
+++ algorithm/src/main/java/io/github/jongminchung/algorithm/codility/binaryGap/Solution.java
@@ -0,0 +1,28 @@
+package io.github.jongminchung.algorithm.codility.binaryGap;
+
+/**
+ * <a href="https://app.codility.com/programmers/lessons/1-iterations/binary_gap">Binary Gap</a>
+ */
+class Solution {
+
+    int solution(int N) {
+        var binary = Integer.toBinaryString(N);
+        var maxGap = 0;
+        var current = 0;
+        var inGap = false;
+
+        for (var c : binary.toCharArray()) {
+            if (c == '1') {
+                if (inGap) {
+                    maxGap = Math.max(maxGap, current);
+                    current = 0;
+                }
+                inGap = true;
+            } else if (inGap) {
+                current++;
+            }
+        }
+
+        return maxGap;
+    }
+}
---
+++ algorithm/src/test/java/io/github/jongminchung/algorithm/codility/binaryGap/SolutionTest.java
@@ -0,0 +1,21 @@
+package io.github.jongminchung.algorithm.codility.binaryGap;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @CsvSource(useHeadersInDisplayName = true, textBlock = """
+            N, EXPECTED
+            32, 0
+            529, 4
+            1041, 5
+            """)
+    void test(int n, int expected) {
+        int result = new Solution().solution(n);
+        assertThat(result).isEqualTo(expected);
+    }
+}
